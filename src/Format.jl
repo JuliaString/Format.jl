44eccc92c8482df84fca88791f5356257dd62ede
ScottPJones <scottjones@alum.mit.edu>
2018-01-23 12:42:06 -0500

    Further updates for recent v0.7 changes

--- src/Format.jl
+++ src/Format.jl
@@ -11,17 +11,10 @@ export pyfmt, cfmt, fmt
 export fmt_default, fmt_default!, reset!, default_spec, default_spec!
 
 # Deal with mess from #16058
-@static if VERSION >= v"0.5.0"
-    const ASCIIStr = String
-    const UTF8Str = String
-    const ByteStr = String
-    const bytestr = String
-else
-    const ASCIIStr = ASCIIString
-    const UTF8Str = UTF8String
-    const ByteStr = ByteString
-    const bytestr = bytestring
-end
+# Later, use Strs package!
+isdefined(Main, :ASCIIStr) || (const ASCIIStr = String)
+isdefined(Main, :UTF8Str)  || (const UTF8Str = String)
+_findfirst(a, b) = Compat.findfirst(equalto(a), b))
 
 include("cformat.jl" )
 include("fmtspec.jl")
