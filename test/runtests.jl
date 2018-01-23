bba58f84b95ea554e0890b16354228ae04e6d6b3
ScottPJones <scottjones@alum.mit.edu>
2018-01-23 07:42:45 -0500

    Add using Random to tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using Format
 
 @static VERSION < v"0.7.0-DEV" ? (using Base.Test) : (using Test)
 @static VERSION >= v"0.7.0-DEV.3058" && (using Printf)
+@static VERSION < v"0.7.0-DEV" || (using Random)
 
 ts(io) = String(take!(io))
 
