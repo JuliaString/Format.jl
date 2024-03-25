0b751ebf1264a57f661995183e16b3fe81fc31e2
ScottPJones <scottjones@alum.mit.edu>
2024-03-25 09:52:18 -0400

    Update cfmt code based on latest changes to Printf.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,7 @@ using Printf
 using Random
 
 @testset "cformat"    begin include( "cformat.jl" ) end
+@testset "printf"     begin include( "printf.jl" ) end
 @testset "fmtspec"    begin include( "fmtspec.jl" ) end
 @testset "formatexpr" begin include( "formatexpr.jl" ) end
 @testset "fmt"        begin include( "fmt.jl" ) end
